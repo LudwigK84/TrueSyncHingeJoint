bd43745124f9fc8057c14f376cad9a00bf674e64
LudwigK84 <Ludwig_Krause@gmx.net>
2017-03-03 13:38:06 +0100

    Extended TSRigidbody with AddRelativeTorque method
    not directly connected to HingeJoints ut I needed it and might be
    interesting for some

---
+++ Assets/NewTSFiles/ExTensionMethods.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TrueSync;
+
+public static class ExTensionMethods
+{
+
+    public static void TSApplyRelativeTorque(this IBody3D thisBody, TSVector torque)
+    {
+        TSVector relCord = TSVector.Transform(torque, thisBody.TSOrientation);
+        thisBody.TSApplyTorque(relCord);
+    }
+
+    public static void AddRelativeTorque(this TSRigidBody thisTSBody, TSVector torque)
+    {
+        thisTSBody.tsCollider.Body.TSApplyRelativeTorque(torque);
+    }
+
+}
---
+++ Assets/TSHingeJointDemo/AddT.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TrueSync;
+
+public class AddT : TrueSyncBehaviour {
+
+    [SerializeField]
+    TSVector Torque;
+    
+    TSRigidBody thisBody;
+
+    public override void OnSyncedStart()
+    {
+        thisBody = GameObject.FindGameObjectWithTag("Bar").GetComponent<TSRigidBody>();
+    }
+
+    public override void OnSyncedInput()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+            TrueSyncInput.SetBool(3, true);
+        else
+            TrueSyncInput.SetBool(3, false);
+    }
+
+    public override void OnSyncedUpdate()
+    {
+        if (TrueSyncInput.GetBool(3))
+        {
+            thisBody.AddRelativeTorque(Torque);
+        }
+    }
+
+}
